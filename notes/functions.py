429b26b4ee1ff6e85fbed1b5c1c8071a1289641a
A9219 <alexey1978malyshev@gmail.com>
2023-09-15 21:24:32 +0300

    control_task1 added

--- notes/functions.py
+++ notes/functions.py
@@ -1,9 +1,20 @@
+import datetime
+from os.path import join, abspath, dirname
+from typing import Tuple, Any
+
+import pandas as pd
+
 MAIN_DIR = abspath(dirname(__file__))
+
 
 def input_note() -> list:
     note_input = []
+    current_date = datetime.datetime.now()
+    current_data_time = current_date.strftime('%m/%d/%y %H:%M:%S')
     note_input.append(input("Введите название заметки: "))
     note_input.append(input("Введите текст заметки: "))
+    note_input.append(current_data_time)
+
 
     return note_input
 
@@ -23,13 +34,14 @@ def export_note_dict_to_csv(notes_loc: dict, file_name: str):
 def read_from_csv(file_name):
     full_name = join(MAIN_DIR, file_name + '.csv')
     df = pd.read_csv(full_name, index_col=False)
+    df.values.sort()
     print(df)
 
 
-def search_note_by_name(notes_loc: dict, searchstr: str) -> int:
+def search_note_by_name(notes_loc: dict, search_str: str) -> tuple[Any, Any]:
     for key, val in notes_loc.items():
-        if val[0].startswith(searchstr):
-            return key
+        if val[0].startswith(search_str):
+            return key, val
 
 
 def delete_note_by_name(notes_loc: dict, delete_note: str):
@@ -39,10 +51,10 @@ def delete_note_by_name(notes_loc: dict, delete_note: str):
             break
 
 
+
 def update_note(notes_loc: dict, up_note_name: str):
     for key, val in notes_loc.items():
         if val[0].startswith(up_note_name):
             val.clear()
             val.append(input("Введите название заметки: "))
             val.append(input("Введите текст заметки: "))
-
--- notes/main.py
+++ notes/main.py
@@ -1,44 +1,32 @@
+# /*Задание
+# Реализовать консольное приложение заметки, с сохранением, чтением,
+# добавлением, редактированием и удалением заметок. Заметка должна
+# содержать идентификатор, заголовок, тело заметки и дату/время создания или
+# последнего изменения заметки. Сохранение заметок необходимо сделать в
+# формате json или csv формат (разделение полей рекомендуется делать через
+# точку с запятой). Реализацию пользовательского интерфейса студент может
+# делать как ему удобнее, можно делать как параметры запуска программы
+# (команда, данные), можно делать как запрос команды с консоли и
+# последующим вводом данных, как-то ещё, на усмотрение студента.
 
-from os.path import join, abspath, dirname
-import pandas as pd
-from functions_notes import functions
 
+import functions
+import datetime
 
 key_count = 0
-phone_dict = {1: ['Иванов', 'Иван', '+7(900)95686596', 'дуралей'],
-              2: ['Петров', 'Петр', '+7(253)565685475', 'умник'],
-              3: ['Соколов', 'Илья', '+7/655/656565848777', 'жадина']}
-
-
-def input_users() -> list:
-    user_input = []
-    user_input.append(input("Введите имя пользователя: "))
-    user_input.append(input("Введите фамилию пользователя: "))
-    user_input.append(input("Введите телефон пользователя: "))
-    user_input.append(input("Введите описание пользователя: "))
-
-    return user_input
-
-
-# print(input_users())
-
-def create(phone_dict_loc: dict, user: list, idc: int) -> dict:
-    idc += 1
-    phone_dict_loc[idc] = user
-    return phone_dict_loc, idc
-
 
 def menu():
-    phone_dict = {1: ['Иванов', 'Иван', '+7(900)95686596', 'дуралей'],
-                  2: ['Петров', 'Петр', '+7(253)565685475', 'умник'],
-                  3: ['Соколов', 'Илья', '+7/655/656565848777', 'жадина'],
-                  4: ['Савельев', 'Илья', '+7*585*5455/65852', 'странный']}
-    key_count = len(phone_dict)
+
+    note_dict = {1: ["first note", "1st note's text", datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S')],
+                  2: ["second note", "2nd note's text", datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S')],
+                 3: ["third note", "3rd note's text", datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S')]}
+    #note_dict = {}
+    key_count = len(note_dict)
     print("Введите 0 ,если хотите выйти ")
-    print("Введите 1 ,если хотите добавить абонента ")
-    print("Введите 2 ,если хотите распечатать справочник ")
-    print("Введите 3 ,если хотите записать данные в файл ")
-    print("Введите 4 для поиска ")
+    print("Введите 1 ,если хотите добавить заметку ")
+    print("Введите 2 ,если хотите распечатать книгу заметок ")
+    print("Введите 3 ,если хотите записать заметку в файл ")
+    print("Введите 4 для поиска по названию ")
     print("Введите 5 для удаления ")
     print("Введите 6 для изменения записи ")
 
@@ -47,84 +35,24 @@ def menu():
         if num == 0:
             break
         if num == 1:
-            user = input_users()
-            phone_dict, key_count = create(phone_dict, user, key_count)
+            note = functions.input_note()
+            note_dict, key_count = functions.create(note_dict, note, key_count)
         if num == 2:
-            print(phone_dict)
+            functions.read_from_csv('test')
+            #print(note_dict)
         if num == 3:
             file_name = input("Введите имя файла: ")
-            export_phone_dict(phone_dict, file_name)
+            functions.export_note_dict_to_csv(note_dict, file_name)
         if num == 4:
-            search = input("Кого ищем? \n")
-            print(search_user(phone_dict, search))
+            search = input("Что ищем? \n")
+            print(functions.search_note_by_name(note_dict, search))
         if num == 5:
-            user_for_del = input("Кого удалим? \n")
-            delete_user(phone_dict, user_for_del)
+            user_for_del = input("Какую запись удалим? \n")
+            functions.delete_note_by_name(note_dict, user_for_del)
         if num == 6:
-            user_for_up = input("Чью запись перекошмарим???\n")
-            update_user(phone_dict, user_for_up)
+            note_for_up = input("Какую запись обновим?\n")
+            functions.note_for_up(note_dict, user_for_up)
 
 
-def export_phone_dict(phone_dict: dict, file_name: str):
-    MAIN_DIR = abspath(dirname(__file__))
-    full_name = join(MAIN_DIR, file_name + '.txt')
-    with open(full_name, mode='w', encoding='utf-8') as file:
-        for key, val in phone_dict.items():
-            file.write(f"{key},{val[0]},{val[1]},{val[2]},{val[3]}\n")
-
-
-def export_phone_dict_to_csv(phone_dict: dict, file_name: str):
-    MAIN_DIR = abspath(dirname(__file__))
-    full_name = join(MAIN_DIR, file_name + '.csv')
-    df = pd.DataFrame(phone_dict)
-    df.to_csv(full_name, index=False, header=True)
-
-def import_from_csv(file_name):
-    MAIN_DIR = abspath(dirname(__file__))
-    full_name = join(MAIN_DIR, file_name + '.csv')
-    df = pd.read_csv(full_name, index_col=False)
-    print(df)
-def search_user(phone_dict: dict, searchstr: str) -> int:
-    for key, val in phone_dict.items():
-        if val[0].startswith(searchstr):
-            return key
-
-
-def delete_user(phone_dict: dict, delete_user: str):
-    for key, val in phone_dict.items():
-        if val[0].startswith(delete_user):
-            del phone_dict[key]
-            break
-
-
-def update_user(phone_dict: dict, up_user: str):
-    for key, val in phone_dict.items():
-        if val[0].startswith(up_user):
-            val.clear()
-            val.append(input("Введите фамилию"))
-            val.append(input("Введите Имя"))
-            val.append(input("Введите тф"))
-            val.append(input("Введите описание"))
-
-
-#
-# print(phone_dict)
-# print(search_user(phone_dict,'д'))
-#menu()
-# export_phone_dict(phone_dict, "phones")
-#export_phone_dict_to_csv(phone_dict, "phones11")
-#import_from_csv("phones11")
-# print(update_user(phone_dict, 'Пе'))
-# print(phone_dict)
-
-
-
-
-
-
-
-
-
-
-
+menu()
 
